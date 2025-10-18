3e7074e921cd80dfdf5f9d68a7980ec360d5d54c
GLuzCode <gladysluz.linaresn@gmail.com>
2025-10-17 19:27:34 -0500

    Actualización para pruebas

--- user-service/src/main/java/com/glprojects/userservice/controller/MessageController.java
+++ user-service/src/main/java/com/glprojects/userservice/controller/MessageController.java
@@ -1,4 +1,19 @@
 package com.glprojects.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
 public class MessageController {
+
+    @Value("${app.message:Mensaje por defecto}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return this.message;
+    }
 }
